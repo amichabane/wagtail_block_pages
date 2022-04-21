76c83a866c7e479e1b4b1b4d7a2f227f366f3306
Amine chabane <chabane.amine@ofppt-edu.ma>
2022-04-21 15:54:40 +0000

    add new page and new block

--- home/models.py
+++ home/models.py
@@ -2,16 +2,20 @@ from wagtail.admin.edit_handlers import StreamFieldPanel
 from wagtail.core.fields import StreamField
 
 from wagtail.core.models import Page
+from wagtail.images.blocks import ImageChooserBlock
 
-from website_blocks.blocks import HeroText
+from website_blocks.blocks import HeroText, CardBlock, QuoteBlock, FeaturedBlock, EmaildBlock
 
 
 class HomePage(Page):
     body = StreamField([
         ('HeroText', HeroText()),
+        ('Cards', CardBlock()),
+        ('Quotes', QuoteBlock()),
+        ('Features', FeaturedBlock()),
+        ('Email', EmaildBlock())
     ], null=True)
 
     content_panels = Page.content_panels + [
         StreamFieldPanel('body'),
     ]
-
--- website_blocks/blocks.py
+++ website_blocks/blocks.py
@@ -1,4 +1,3 @@
-
 from wagtail.core import blocks
 from wagtail.images.blocks import ImageChooserBlock
 
@@ -7,8 +6,65 @@ class HeroText(blocks.StructBlock):
     title = blocks.RichTextBlock(default="Title", required=False)
     description = blocks.RichTextBlock(default="Description", required=False)
     button = blocks.CharBlock(default="Button", required=False)
+    image=ImageChooserBlock()
 
     class Meta:
         template = 'website_blocks/hero_text.html'
         icon = 'image'
 
+
+class CardBlock(blocks.StructBlock):
+    title = blocks.CharBlock(required=False, help_text="Add your title")
+    text = blocks.RichTextBlock(required=False, help_text="Add your title")
+
+    cards = blocks.ListBlock(
+        blocks.StructBlock(
+            [
+                ("image", ImageChooserBlock(required=False)),
+                ("title", blocks.CharBlock(required=False, max_length=40)),
+                ("text", blocks.TextBlock(required=False, max_length=200)),
+                ("button_page", blocks.PageChooserBlock(required=False)),
+                ("button_url", blocks.URLBlock(required=False,
+                                               help_text="If the button page above is selected, that will be used first.")),
+            ]
+        )
+    )
+
+    class Meta:  # noqa
+        template = "website_blocks/card_block.html"
+        icon = "placeholder"
+
+
+class QuoteBlock(blocks.StructBlock):
+
+    text = blocks.RichTextBlock(required=False, help_text="Add your text")
+    image=ImageChooserBlock()
+    username=blocks.CharBlock(required=False,help_text="Add Your Username")
+    job=blocks.CharBlock(required=False,help_text="Add Your Job")
+
+    class Meta:  # noqa
+        template = "website_blocks/quote_block.html"
+        icon = "placeholder"
+
+
+class FeaturedBlock(blocks.StructBlock):
+
+    title = blocks.CharBlock(required=False, help_text="Add Your title")
+    text = blocks.RichTextBlock(required=False, help_text="Add your text")
+    image=ImageChooserBlock()
+    subtitle=blocks.CharBlock(required=False,help_text="Add Your Username")
+
+    class Meta:  # noqa
+        template = "website_blocks/featured.html"
+        icon = "placeholder"
+
+
+class EmaildBlock(blocks.StructBlock):
+
+    title = blocks.CharBlock(required=False, help_text="Add Your title")
+    description = blocks.RichTextBlock(required=False, help_text="Add your text")
+    subtexte=blocks.RichTextBlock(required=False,help_text="Add Your SubText")
+
+    class Meta:  # noqa
+        template = "website_blocks/Email.html"
+        icon = "placeholder"
